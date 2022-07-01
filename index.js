f8163d31aaf1b064dbc4cefa95517c03c976dd9d
HuynhNam <ctnam789@gmail.com>
2022-07-01 23:30:10 +0700

    up1

---
+++ index.js
@@ -0,0 +1,102 @@
+// Câu 1
+
+// SƠ ĐỒ 3 KHỐI
+
+// Input:lương 1 ngày+số ngày làm
+
+// Process :1 lấy Input
+// 2 tính lương=1 ngày lương*số ngày làm
+// 3 log luong ra man hình
+
+// Output :lương
+
+var salaryDay=10000
+var workingDays=25
+var totalSlary=salaryDay*workingDays
+
+console.log('tổng lương tháng là:',totalSlary)
+
+
+// Câu 2
+
+// SƠ ĐỒ 3 KHỐI
+
+// Input:5 số thực
+
+// Process :1 lấy Input
+// 2 tính giá trị trung bình của 5 số
+// 3 log kết quả ra man hình
+
+// Output :trung bình tổng 5 số
+
+var number1=1
+var number2=2
+var number3=3
+var number4=4
+var number5=5
+var averageNumber=(number1+number2+number3+number4+number5)/5
+
+console.log('trung bình tổng 5 số là',averageNumber)
+
+
+
+// Câu 3
+
+// SƠ ĐỒ 3 KHỐI
+
+// Input:giá USD 23.500, số tiền USD
+
+// Process :1 lấy Input
+// 2 tính giá USD ra VND
+// 3 log kết quả ra man hình
+
+// Output :giá VND
+
+var priceUSD=23500
+var moneyUSD=2
+var priceVND=moneyUSD*priceUSD
+console.log('giá tiền của 2 USD là',priceVND)
+
+// Câu 4
+
+// SƠ ĐỒ 3 KHỐI
+
+// Input:chiều dài+chiều roog của HCN
+
+// Process :1 lấy Input
+// 2 tính chu vi và diện tích hcn
+// 3 log kết quả ra man hình
+
+// Output :chu vi và diện tích hcn
+
+var longR=10
+var wideR=5
+var areaR=longR*wideR
+var perR=(longR+wideR)*2
+console.log('dien tích HCN là',areaR)
+console.log('chu vi HCN là',perR)
+
+
+
+// Câu 5
+
+// SƠ ĐỒ 3 KHỐI
+
+// Input:1 số có 2 chữ số
+
+// Process :1 lấy Input
+// 2 tính tổng 2 ký số
+// 3 log kết quả ra man hình
+
+// Output :tổng 2 ký số 
+
+var numberAB=25
+var numberB=numberAB%10
+var numberA=(numberAB-numberB)/10
+var totalAB=numberA+numberB
+console.log('tổng 2 ký số là',totalAB)
+
+
+
+
+
